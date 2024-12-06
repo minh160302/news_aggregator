44b6e20d7cf594ad2443dc362e5c60510c995f1a
Minh Nguyen <44615298+minh160302@users.noreply.github.com>
2024-12-05 18:29:28 -0800

    update port

--- src/main.py
+++ src/main.py
@@ -112,4 +112,4 @@ async def summarize_article_url(body: ArticleUrlBody):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
