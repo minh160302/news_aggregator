855e3aaa0eb2798fadb5b2024c9b318ec79b3d03
Minh Nguyen <44615298+minh160302@users.noreply.github.com>
2024-12-05 18:18:13 -0800

    update main

--- src/main.py
+++ src/main.py
@@ -107,3 +107,9 @@ async def summarize_article_url(body: ArticleUrlBody):
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Redis read error: {str(e)}")
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
